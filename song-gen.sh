458929b99c64a25969a068a5942f7d075060cbca
gregrr@gmail.com <gregrr@gmail.com>
2022-12-14 13:48:14 -0500

    added auto word seeding; made outputs quiet

--- song-gen.sh
+++ song-gen.sh
@@ -1,11 +1,20 @@
 source config.sh
-curl https://api.openai.com/v1/completions \
+
+if [ ! -f "popular.txt" ]; then
+    echo "Downloading Dictonary File; Only happens once!"
+    curl --silent https://raw.githubusercontent.com/dolph/dictionary/master/popular.txt > popular.txt
+fi
+
+prompt="Write Hotdog verse about $( shuf -n 1 popular.txt )"
+
+echo $prompt
+curl --silent https://api.openai.com/v1/completions \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $apikey" \
   -d '{
         "model": "text-davinci-003",
-        "prompt": "Write Hotdog verse about dancing",
-        "temperature": 0.9,
+        "prompt": "'"$prompt"'",
+        "temperature": 1,
         "max_tokens": 150,
         "top_p": 1,
         "frequency_penalty": 0.0,
@@ -21,8 +30,10 @@ perl -pe   's/"text":\s"//; \
             s/\\n/\n/g' | \
 gtts-cli - --output verse.wav
 
-
 #casio beat taken from https://audiokitpro.com/free-toy-casio-loops/
-ffmpeg -i verse.wav -i beat_long.mp3 -filter_complex amix=inputs=2:duration=shortest -b:a 160k song_$( date +%s ).mp3
-
+output="song_$( date +%s ).mp3"
+ffmpeg -hide_banner -loglevel error -i verse.wav -i beat_long.mp3 -filter_complex amix=inputs=2:duration=shortest -b:a 160k $output
 rm verse.wav
+echo "Done: $output"
+
+
