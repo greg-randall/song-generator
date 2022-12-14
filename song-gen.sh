ca6e6f157548c8a2e222762ac9f36fc8ec336c6c
gregrr@gmail.com <gregrr@gmail.com>
2022-12-14 14:14:52 -0500

    formatting

--- song-gen.sh
+++ song-gen.sh
@@ -6,9 +6,11 @@ if [ ! -f "popular.txt" ]; then
 fi
 
 seed_word=$( shuf -n 1 popular.txt )
-prompt="Write Hotdog verse about $seed_word"
+prompt="Write Hotdog verse about $seed_word:"
+time=$( date +%s )
+output="song-$seed_word-$time"
 
-echo $prompt
+echo -e "$prompt\n"
 curl --silent https://api.openai.com/v1/completions \
   -H "Content-Type: application/json" \
   -H "Authorization: Bearer $apikey" \
@@ -28,11 +30,14 @@ perl -pe   's/"text":\s"//; \
             s/\s*",$//; \
             s/\\n\\n//; \
             s/^\s+//; \
-            s/\\n/\n/g' | \
-gtts-cli - --output verse.wav
+            s/\\n/\n/g' > $output.txt
+
+cat $output.txt
+
+gtts-cli -f $output.txt --output verse.wav
 
 #casio beat taken from https://audiokitpro.com/free-toy-casio-loops/
-output="song-$seed_word-$( date +%s ).mp3"
-ffmpeg -hide_banner -loglevel error -i verse.wav -i beat_long.mp3 -filter_complex amix=inputs=2:duration=shortest -b:a 160k $output
+
+ffmpeg -hide_banner -loglevel error -i verse.wav -i beat_long.mp3 -filter_complex amix=inputs=2:duration=shortest -b:a 160k $output.mp3
 rm verse.wav
-echo "Done: $output"
+echo -e "\nDone: $output.mp3"
