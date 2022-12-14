0f488ddffaf032dce90b850b6782e10f1a3d7360
gregrr@gmail.com <gregrr@gmail.com>
2022-12-14 14:02:51 -0500

    filenames

--- song-gen.sh
+++ song-gen.sh
@@ -5,7 +5,8 @@ if [ ! -f "popular.txt" ]; then
     curl --silent https://raw.githubusercontent.com/dolph/dictionary/master/popular.txt > popular.txt
 fi
 
-prompt="Write Hotdog verse about $( shuf -n 1 popular.txt )"
+seed_word=$( shuf -n 1 popular.txt )
+prompt="Write Hotdog verse about $seed_word"
 
 echo $prompt
 curl --silent https://api.openai.com/v1/completions \
@@ -31,9 +32,7 @@ perl -pe   's/"text":\s"//; \
 gtts-cli - --output verse.wav
 
 #casio beat taken from https://audiokitpro.com/free-toy-casio-loops/
-output="song_$( date +%s ).mp3"
+output="song-$seed_word-$( date +%s ).mp3"
 ffmpeg -hide_banner -loglevel error -i verse.wav -i beat_long.mp3 -filter_complex amix=inputs=2:duration=shortest -b:a 160k $output
 rm verse.wav
 echo "Done: $output"
-
-
